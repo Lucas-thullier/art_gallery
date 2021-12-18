8b7f5a5bec87e5bd2eee535e1c23f103bca816c1
lucas <lucas.thullier@outlook.fr>
2021-12-18 12:10:38 +0000

    more precise config for cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -138,10 +138,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     os.environ.get('FRONTEND_URL'),
-# ]
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get('FRONTEND_URL'),
+]
 
 
 # Handle logs

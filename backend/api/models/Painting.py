aa60fbd6560dedde58da361752451c384983cfe2
lucas <lucas.thullier@outlook.fr>
2021-12-29 15:07:18 +0000

    change save hook for inception_at and decorator seem to be a best way to hook on model event than model constructor

--- backend/api/models/Painting.py
+++ backend/api/models/Painting.py
@@ -1,37 +1,21 @@
 from django.db import models
 from django.utils.timezone import now
-
-
-from .Creator import Creator
-from .Depiction import Depiction
-from .Genre import Genre
-from .Location import Location
-from .Material import Material
-from .Movement import Movement
+import re
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from . import Creator, Depiction, Genre, Location, Material, Movement
 from ..repositories import PaintingQuerySet
 
 
 class Painting(models.Model):
-    def __init__(self, *args, **kwargs):
-        if 'width' in kwargs:
-            kwargs['width'] = float(kwargs['width'])
-        else:
-            kwargs['width'] = None
-
-        if 'height' in kwargs:
-            kwargs['height'] = float(kwargs['height'])
-        else:
-            kwargs['height'] = None
-        super().__init__(*args, **kwargs)
-
     name = models.CharField(max_length=500, null=True)
     native_name = models.CharField(max_length=500, null=True)
     wikidata_url = models.CharField(max_length=500, unique=True, null=False)
 
     title = models.CharField(max_length=500, null=True)
-    picture_url = models.CharField(max_length=500, null=True)
+    picture_url = models.CharField(max_length=1000, null=True)
     owned_by = models.CharField(max_length=500, null=True)
-    inception_at = models.DateTimeField(null=True)
+    inception_at = models.CharField(max_length=10, null=True)
     width = models.PositiveIntegerField(null=True)
     height = models.PositiveIntegerField(null=True)
     described_at = models.CharField(max_length=500, null=True)
@@ -49,3 +33,25 @@ class Painting(models.Model):
     updated_at = models.DateTimeField(default=now)
 
     objects: models.Manager = PaintingQuerySet.as_manager()
+
+
+@receiver(pre_save, sender=Painting)
+def painting_pre_save(sender, instance, *args, **kwargs):
+    if instance.width is not None:
+        instance.width = float(instance.width)
+    else:
+        instance.width = None
+
+    if instance.height is not None:
+        instance.height = float(instance.height)
+    else:
+        instance.height = None
+
+    if type(instance.inception_at) is str:
+        if instance.inception_at.startswith('http'):
+            instance.inception_at = None
+        else:
+            year_regex = r"^-?\d+(?=-)"
+            match = re.match(year_regex, instance.inception_at)
+            if match is not None:
+                instance.inception_at = match.group(0)

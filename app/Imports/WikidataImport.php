3d3dd00767919a08887be84a4a8d2748c0cc46f6
lucas <lucas.thullier@outlook.fr>
2022-01-22 11:33:01 +0100

    working on import process from wikidata

---
+++ app/Imports/WikidataImport.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Painting;
+use Wikidata\Wikidata;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Storage;
+
+class WikidataImport
+{
+  public function fullImport()
+  {
+    // $xmlPaintingsList = $this->getPaintingsListXml();
+    $xmlPaintingsList = Storage::disk('public')->get('sparql_test'); //pour test
+
+    $paintingsIds = $this->extractPaintingsId($xmlPaintingsList);
+
+    foreach ($paintingsIds as $paintingId) {
+      Painting::firstOrCreate(
+        ['wikidata_id' => $paintingId],
+        ['wikidata_id' => $paintingId]
+      );
+    }
+  }
+
+  public function getPaintingsListXml(): ?string
+  {
+    $client = new Client();
+    $response = $client->get('https://query.wikidata.org/sparql?query=SELECT%20DISTINCT%20?item%20WHERE%20{%20?item%20p:P31%20?statement0.%20?statement0%20(ps:P31/(wdt:P279*))%20wd:Q3305213.%20}');
+    if ($response->getStatusCode() === 200) {
+      $xmlPaintingsList = (string) $response->getBody();
+      return $xmlPaintingsList;
+    } else {
+      return null;
+    }
+  }
+
+  private function extractPaintingsId(string $xmlPaintingsList): array
+  {
+    $dom = new \DOMDocument();
+    $dom->loadXML($xmlPaintingsList);
+
+    $paintingsUrlAsDomList = $dom->getElementsByTagName('uri');
+
+    $paintingsIds = [];
+    foreach ($paintingsUrlAsDomList as $paintingUrlAsDomNode) {
+      $paintingId = preg_replace('#.+entity\/#', '', $paintingUrlAsDomNode->nodeValue);
+
+      if (trim($paintingId) != '') {
+        $paintingsIds[] = $paintingId;
+      }
+    }
+
+    return $paintingsIds;
+  }
+}
